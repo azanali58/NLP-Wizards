cd5de955aab7a9161573c6293463221d51edb397
Ahsan Ali <ahsan10041@gmail.com>
2023-11-06 00:36:53 +0100

    NW-16 <Implemented PDF generation>

--- app.py
+++ app.py
@@ -10,6 +10,14 @@ from langchain.vectorstores import FAISS
 from langchain.chains import ConversationalRetrievalChain
 from langchain.chat_models import ChatOpenAI
 
+def get_pdf_text(pdf_docs):
+    text = ""
+    for pdf in pdf_docs:
+        pdf_reader = PdfReader(pdf)
+        for page in pdf_reader.pages:
+            text += page.extract_text()
+    return text
+
 def main():
     load_dotenv()
     st.set_page_config(
@@ -23,6 +31,14 @@ def main():
     with st.sidebar:
         st.subheader("Your transcript(s)")
         pdf_docs = st.file_uploader("Upload your transcript PDFs here and click 'Process'", accept_multiple_files=True)
-        st.button("Process")
+        
+        if st.button("Process"):
+            with st.spinner("Processing"):
+
+                # get pdf text
+                raw_text = get_pdf_text(pdf_docs)
+
+
+                
 if __name__ == "__main__":
     main()
