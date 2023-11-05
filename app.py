eac12687c1833fccda16cf4ad144e257f3b1f413
Ahsan Ali <ahsan10041@gmail.com>
2023-11-06 00:39:42 +0100

    NW-17 <Text Chunking>

--- app.py
+++ app.py
@@ -18,6 +18,17 @@ def get_pdf_text(pdf_docs):
             text += page.extract_text()
     return text
 
+def get_text_chunks(text):
+    text_splitter = CharacterTextSplitter(
+        separator="\n",
+        chunk_size=1000,
+        chunk_overlap=200,
+        length_function=len
+    )
+    chunks = text_splitter.split_text(text)
+    return chunks
+
+
 def main():
     load_dotenv()
     st.set_page_config(
@@ -38,7 +49,10 @@ def main():
                 # get pdf text
                 raw_text = get_pdf_text(pdf_docs)
 
+                # get text chunks
+                text_chunks = get_text_chunks(raw_text)
 
-                
+
+
 if __name__ == "__main__":
     main()
