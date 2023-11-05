b16bc9b0f7ef124ea02a3849d14a4826e0e74371
Ahsan Ali <ahsan10041@gmail.com>
2023-11-06 00:29:47 +0100

    NW-15 <Completed API integration>

--- app.py
+++ app.py
@@ -1,6 +1,17 @@
 import streamlit as st
 
+from dotenv import load_dotenv
+from PyPDF2 import PdfReader
+from langchain.text_splitter import CharacterTextSplitter
+from langchain.chat_models import ChatOpenAI
+from langchain.embeddings import OpenAIEmbeddings
+from langchain.memory import ConversationBufferMemory
+from langchain.vectorstores import FAISS
+from langchain.chains import ConversationalRetrievalChain
+from langchain.chat_models import ChatOpenAI
+
 def main():
+    load_dotenv()
     st.set_page_config(
         page_title="NLP Wizard Chatbot",
         layout="wide",
